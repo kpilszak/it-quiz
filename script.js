5d39487a7d4851aa8bab07ea9030d8f73d834b70
kpilszak <klaudia.pilszak@gmail.com>
2019-05-06 06:55:38 +0200

    feat: Select all checkboxes

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 var points = 0;
+
 var correct = document.querySelectorAll('.correct');
 console.log(correct);
+
+var answerCheckboxes = document.getElementsByTagName('input');
+console.log(answerCheckboxes);
